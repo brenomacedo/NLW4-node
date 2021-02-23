b6363ba99e372fc3fa00f98059670d13453d6606
brenosekiro <b88660501@gmail.com>
2021-02-23 12:40:32 -0300

    userMigration

---
+++ src/database/migrations/1614088600104-CreateUsers.ts
@@ -0,0 +1,33 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm"
+
+export class CreateUsers1614088600104 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "users",
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true
+                }, {
+                    name: 'name',
+                    type: 'varchar'
+                }, {
+                    name: 'email',
+                    type: 'varchar'
+                }, {
+                    name: 'createdAt',
+                    type: 'timestamp',
+                    default: 'now()'
+                }
+            ]
+
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users')
+    }
+
+}
